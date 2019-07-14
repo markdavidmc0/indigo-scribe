3cbacf4152ef77bb1fb679e1c748ffc882755cbb
markdavidmc0 <mark@dotmodus.com>
2019-07-14 17:01:36 +0200

    # added base project endpoint and block content endpoint (file uploads to GCS via file field)

--- indigoscribe/myproject/serializers.py
+++ indigoscribe/myproject/serializers.py
@@ -1,3 +1,20 @@
 """Serializers for MyProject app."""
 from rest_framework import serializers
 
+from myproject.models import ContentBlock, Project
+
+
+class ContentBlockSerializer(serializers.ModelSerializer):
+    """Serializer for Content Block objects."""
+    class Meta:
+        """Association between serializer and model."""
+        model = ContentBlock
+        fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    """Serializer for Projects."""
+    class Meta:
+        """Association between serializer and model."""
+        model = Project
+        fields = '__all__'
--- indigoscribe/myproject/views.py
+++ indigoscribe/myproject/views.py
@@ -1,28 +1,29 @@
-from django.core.files.storage import default_storage
-from django.http import HttpResponse
-from django.contrib.auth.models import User
+from environs import Env
+
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-GOOGLE_APPLICATION_CREDENTIALS = '/Users/markmc/Repos/sandbox/indigo-scribe'
+from myproject.models import ContentBlock, Project
+from myproject.serializers import ContentBlockSerializer, ProjectSerializer
+
+env = Env()
+env.read_env()  # read .env file, if it exists
+
+GOOGLE_APPLICATION_CREDENTIALS = env('GOOGLE_APPLICATION_CREDENTIALS')
+
+
+def index(request) -> Response:
+    """Landing page."""
+    return Response("Hello, world. You're at the My Project landing page.")
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     """A viewset for interacting with project objects."""
-    queryset = User.objects.all()
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 
 class ContentBlockViewSet(viewsets.ModelViewSet):
     """A viewset for managing content blocks."""
-    queryset = User.objects.all()
-
-    def store(self):
-        """File handling for content blocks."""
-        test = default_storage.exists('storage_test')
-        return Response(test)
-
-
-def index(request):
-    """Landing page."""
-    return HttpResponse("Hello, world. You're at the My Project landing page.")
-
+    queryset = ContentBlock.objects.all()
+    serializer_class = ContentBlockSerializer
